a6a8878b86c7a17d1c7a5c90c15de5fb297a3cd4
Liz <liz-delrio@outlook.com>
2021-07-03 13:24:03 -0500

    se arregla la funcion que limpia el input nombre

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import validator from './validator.js';
-
+/* Función que permite manipular los display de la pantalla inicial
+  y final. Tambien obtener e imprimir los datos de los imput*/
 const screen1 = () => {
   let finalSreen = document.getElementById("finalScreen");
   finalSreen.style.display = "block";
@@ -14,21 +15,25 @@ const screen1 = () => {
   document.getElementById('cardNumber').innerHTML = creditCardNumber;
 }
 
+/* Función que permite manipular los display de la pantalla final
+  e inicial. Tambien limpiar los campos imput*/
 const screen2 = () => {
   let finalSreen = document.getElementById("finalScreen");
   finalSreen.style.display = "none";
 
   let homeScreen = document.getElementById('homeScreen');
   homeScreen.style.display = "block"
-  
-  document.getElementById('name').velue = "";
+
+  document.getElementById('name').value = "";
   document.getElementById('cardN').value = "";
 
 }
 
+// Funcionalidad del boton validar
 let btnValidar = document.getElementById('validar');
 btnValidar.addEventListener("click", screen1);
 
+// Funcionalidad del boton validar otra tarjeta
 let btnOtherCC = document.getElementById('btn2');
 btnOtherCC.addEventListener("click", screen2);
 
