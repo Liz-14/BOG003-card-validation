7ba20a73a7abcc5a4f10c2a7587eb71a8c4babe3
Liz <liz-delrio@outlook.com>
2021-07-05 11:53:46 -0500

    Se creo la clase validator que rebice el numero de tarjeta como parametro y permite validarla y enmascararla

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,72 @@
-const validator = {
-  // ...
-};
-console.log("hola Mundo");
+function validator (creditCardNumber){
+  this.creditCardNumber = creditCardNumber;
+  this.valid = false;
+  this.numValid = /[^0-9-\s]+/.test(creditCardNumber); // regresa un booleano
+  this.isValid = () => {
+
+    if(this.numValid === true){
+      return alert("Por favor solo escribe numeros");
+    }
+    else {
+      let cNumber = this.creditCardNumber;
+      let oddPosNum = 0;
+      let evenPosNum = 0;
+      let sumEven1 = 0;
+      let sumEven10 = 0;
+      let sumEven2 = 0;
+      let totalSum = 0;
+
+      cNumber = cNumber.replace(/\D/g, "");
+      /*bucle para reccorer el elemento "creditCardNumber" de derecha a iazquierda (Final->Inicio)*/
+      //"i" respresenta la pos de cada elemnto
+      for(let i = cNumber.length - 1; i >= 0; i--){
+
+        //Escoge aquellos numeros que tienen posicion impar
+        if(i % 2 != 0){
+          oddPosNum = parseInt(cNumber.charAt(i))*2;
+          oddPosNum = oddPosNum.toString();
+          //Escoge aquellos numeros que en la operacion anterior sean mayores a 10 y los suma entre si
+          if(oddPosNum >= 10){
+            sumEven10 = parseInt(oddPosNum.charAt(0)) + parseInt(oddPosNum.charAt(1));
+            sumEven1 += sumEven10;
+          }
+          else {
+            sumEven2 += parseInt(oddPosNum);
+          }
+        }
+        //hace la suma de todos los numeros que tienen psociion impar
+        else {
+          evenPosNum += parseInt(cNumber.charAt(i));
+        }
+      }
+      //Suma todos los elementos resultado de las operaciones anteriores
+      totalSum = sumEven1 + sumEven2 + evenPosNum;
+
+      //Validacion
+      if(totalSum % 10 == 0){
+        let answer = "Valida"
+        let valid = this.valid = true;
+        return {answer, valid};
+      }
+      else {
+        let answer = "Invalida"
+        let valid = this.valid = false;
+        return {answer, valid};
+      }
+    }
+  }
+
+  this.maskify = () => {
+    let cNumber = this.creditCardNumber;
+    //se extraen solo los ultimos 4 numeros
+    let lastNumbers = cNumber.substring(cNumber.length-4);
+    //Se extraen todos los numeros menos los 4 ultimos
+    let initialNumbers = cNumber.substring(0,cNumber.length-4);
+    // Se enmascara con # para /./-> cualquier carácter
+    let cNumberFinalScreen = initialNumbers.replace(/./g, '#') + " " + lastNumbers;
+    // retorna un objeto
+    return {lastNumbers, initialNumbers, cNumberFinalScreen};
+  }
+}
+
 export default validator;
